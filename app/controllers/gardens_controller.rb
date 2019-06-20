ffbbfa3caf5491295614249c378f15f9675d3d33
darrendaz <darren.lou@gmail.com>
2019-06-20 13:20:52 -0700

    added a new html template for gardens, WIP new and create controllers for gardensController

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,6 +1,23 @@
 class GardensController < ApplicationController
   before_action :authenticate_user!
+  def new
+    @garden = Garden.new
+  end
+
+  def create
+    garden = current_user.gardens.build(garden_params)
+    binding.pry
+    if garden.save
+      redirect_to gardens_path
+    end
+  end
+
   def index
     @gardens = Garden.all
+  end
+
+  private
+  def garden_params
+    params.require(:garden).permit(:name)
   end
 end
