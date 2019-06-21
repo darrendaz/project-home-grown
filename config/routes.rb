7b2f56721786b63e0d36fb77265fdb46f3d77a1a
darrendaz <darren.lou@gmail.com>
2019-06-21 15:32:21 -0700

    generate devise views

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   end
 
   devise_scope :user do
-    get 'signup', to 'devise/registrations#new'
+    get 'signup', to: 'devise/registrations#new'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
