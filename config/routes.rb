e28ce35562e7b446de38644898aae28a015ca782
darrendaz <darren.lou@gmail.com>
2019-06-20 17:25:20 -0700

    add index action to plants controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   root "application#index"
   
-  resources :gardens
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions', gardens: 'users/gardens' }
   resources :users, only: [:show] do
     resources :gardens, only: [:index, :show]
   end
+  resources :gardens, only: [:show] do
+    resources :plants, only: [:index, :show]
+  end
+  resources :gardens
+  resources :plants
 
 end
