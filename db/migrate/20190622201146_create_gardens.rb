e105df29a708e7bd245d8fe3ebaa052290edf829
darrendaz <darren.lou@gmail.com>
2019-06-22 15:03:14 -0700

    remove belongs_to plant from gardens migration

--- db/migrate/20190622201146_create_gardens.rb
+++ db/migrate/20190622201146_create_gardens.rb
@@ -5,7 +5,6 @@ class CreateGardens < ActiveRecord::Migration[5.2]
       t.datetime :start_date
       t.datetime :end_date
       t.belongs_to :user
-      t.belongs_to :plant
       t.timestamps
     end
   end
