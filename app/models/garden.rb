ebc068e9ac9f0d1af41b316fbf77fea91b0394f0
darrendaz <darren.lou@gmail.com>
2019-06-22 13:24:30 -0700

    generate plant model

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,2 +1,3 @@
 class Garden < ApplicationRecord
+  
 end
--- db/migrate/20190622201146_create_gardens.rb
+++ db/migrate/20190622201146_create_gardens.rb
@@ -1,7 +1,9 @@
 class CreateGardens < ActiveRecord::Migration[5.2]
   def change
     create_table :gardens do |t|
-
+      t.string :name
+      t.datetime :start_date
+      t.datetime :end_date
       t.timestamps
     end
   end
