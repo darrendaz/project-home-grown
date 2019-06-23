cd59324a2ce2275d0cfbc96b05e1dbdb033c3c89
darrendaz <darren.lou@gmail.com>
2019-06-22 22:17:56 -0700

    added remove column user_id from gardens table to migration

--- db/migrate/20190623042114_remove_garden_column_from_plants.rb
+++ db/migrate/20190623042114_remove_garden_column_from_plants.rb
@@ -1,5 +1,7 @@
 class RemoveGardenColumnFromPlants < ActiveRecord::Migration[5.2]
   def change
-    remove_column :plants, :garden
+    remove_column :plants, :garden_id
+
+    remove_column :gardens, :user_id
   end
 end
