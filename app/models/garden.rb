602277887c5ba5271d6473a086150e9fd7374c19
darrendaz <darren.lou@gmail.com>
2019-06-22 23:20:02 -0700

    save garden.rb

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,7 +1,7 @@
 class Garden < ApplicationRecord
 
-  has_many :users_gardens, class_name: "UserGardens"
-  has_many :users, through: :users_gardens
+  has_many :user_gardens, class_name: "UserGardens"
+  has_many :users, through: :user_gardens
   has_many :gardens_plants
   has_many :plants, through: :gardens_plants
   has_many :comments, through: :plants
