1905971a251ce71059d17df865462c7d9ff441a0
darrendaz <darren.lou@gmail.com>
2019-06-22 23:40:03 -0700

    Migrate changes to gardens_plants to rename to gardens_plant

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -3,6 +3,6 @@ class Garden < ApplicationRecord
   has_many :user_gardens, class_name: "UserGardens"
   has_many :users, through: :user_gardens
   has_many :gardens_plant, class_name: "GardensPlant"
-  has_many :plants, through: :gardens_plants
+  has_many :plants, through: :gardens_plant
   has_many :comments, through: :plants
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_23_061752) do
+ActiveRecord::Schema.define(version: 2019_06_23_063134) do
 
   create_table "comments", force: :cascade do |t|
     t.text "contents"
@@ -30,13 +30,13 @@ ActiveRecord::Schema.define(version: 2019_06_23_061752) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "gardens_plants", force: :cascade do |t|
+  create_table "gardens_plant", force: :cascade do |t|
     t.integer "garden_id"
     t.integer "plant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["garden_id"], name: "index_gardens_plants_on_garden_id"
-    t.index ["plant_id"], name: "index_gardens_plants_on_plant_id"
+    t.index ["garden_id"], name: "index_gardens_plant_on_garden_id"
+    t.index ["plant_id"], name: "index_gardens_plant_on_plant_id"
   end
 
   create_table "plants", force: :cascade do |t|
