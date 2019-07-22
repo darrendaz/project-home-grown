ca1d9bb7e5bdb07de29b01a62369225d93d23405
darrendaz <darren.lou@gmail.com>
2019-07-22 11:09:42 -0700

    garden.jsGarden class constructor

--- app/assets/javascripts/garden.js
+++ app/assets/javascripts/garden.js
@@ -13,9 +13,18 @@ function listenForClick() {
 function getGarden() {
   $.ajax({
     url: "http://localhost:3000/gardens",
-    method: "get",
-    dataType: "json"
+    method: 'get',
+    dataType: 'json'
   }).done(function (data) {
     console.log("our data is:", data)
   })
+}
+
+class Garden {
+  constructor(object) {
+    this.id = object.id
+    this.name = object.name
+    this.start_date = object.start_date
+    this.end_date = object.end_date
+  }
 }
