9799346a5897ae419aeed9c1f096be6f27612919
darrendaz <darren.lou@gmail.com>
2019-07-24 15:16:09 -0700

    implement created_at timestamp to be readable format

--- app/assets/javascripts/plant.js
+++ app/assets/javascripts/plant.js
@@ -1,7 +1,15 @@
 $(function () {
   console.log("Plant is working")
   getPlant()
+  bindClickHandlers()
 })
+  ;
+const bindClickHandlers = () => {
+  $('#new_comment').on("submit", function (e) {
+    e.preventDefault()
+    console.log($(this).serialize())
+  })
+}
 
 function getPlant() {
   if ($("#plant").data("id")) {
@@ -34,8 +42,16 @@ class Plant {
 }
 
 Plant.prototype.plantHTML = function () {
+  let plantCommentsHTML = this.comments.map(comment => {
+    let timeStamp = new Date(Date.parse(comment.created_at))
+    return (`
+      <li>
+        <small><b>${timeStamp}</b>: ${comment.contents}</small>
+      </li>
+    `)
+  }).join('')
   return (`
-    <div>
+    <div id="plant-data">
       <h2>I'm rendering a plant: ${this.name}</h2>
       <p>Name: ${this.name}</p>
       <p>Species: ${this.species}</p>
@@ -43,6 +59,12 @@ Plant.prototype.plantHTML = function () {
       <p>Type: ${this.type}</p>
       <p>Sex: ${this.sex}</p>
       <p>Time Until Harvest: ${this.time_until_harvest}</p>
+      <div id="plant-comments">
+      <h3>Comments:</h3>
+        <ul>
+          ${plantCommentsHTML}
+        </ul>
+      <div>
     </div>
   `)
 }
--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,5 +1,5 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :contents, :plant_id
+  attributes :id, :contents, :plant_id, :created_at
 
   belongs_to :plant
 end
