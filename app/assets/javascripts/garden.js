430c7d0a4e7390cd1495cc3380a536113d80a99f
darrendaz <darren.lou@gmail.com>
2019-07-25 17:41:44 -0700

    created gardensRequestCallback function in garden.js and abstracted repetitive variables using const

--- app/assets/javascripts/garden.js
+++ app/assets/javascripts/garden.js
@@ -4,38 +4,36 @@ $(function () {
 
 function getGarden() {
   const base_url = 'http://localhost:3000/gardens/'
+
   if ($("#gardens").data("id")) {
     const user_id = $("#gardens").data("id")
     const user_url = 'http://localhost:3000/users/' + user_id + '/gardens'
-    let request_url = (user_id) ? user_url : base_url
+
     $.ajax({
-      url: request_url,
+      url: user_url,
       method: 'get',
       dataType: 'json',
-    }).done(function (data) {
-      for (let item of data) {
-        let firstGarden = new Garden(item);
-        let myGardenHTML = firstGarden.gardenHTML()
-        if (document.getElementById("gardens")) {
-          document.getElementById("gardens").innerHTML += myGardenHTML;
-        }
-      }
-    })
+    }).done(data => gardensRequestCallback(data))
+
   } else {
     $.ajax({
       url: base_url,
       method: 'get',
       dataType: 'json',
-    }).done(function (data) {
-      for (let item of data) {
-        let firstGarden = new Garden(item);
-        let myGardenHTML = firstGarden.gardenHTML()
-        let element = document.getElementById("gardens")
-        if (element) {
-          document.getElementById("gardens").innerHTML += myGardenHTML;
-        }
-      }
-    })
+    }).done(data => gardensRequestCallback(data))
+  }
+}
+
+function gardensRequestCallback(data) {
+  const element = document.getElementById("gardens")
+
+  for (let item of data) {
+    let garden = new Garden(item);
+    let gardenListHTML = garden.gardenHTML()
+
+    if (element) {
+      element.innerHTML += gardenListHTML;
+    }
   }
 }
 
@@ -55,6 +53,7 @@ Garden.prototype.gardenHTML = function () {
       <p><a href="/plants/${plant.id}">${plant.name}</a ></p >
       `)
   }).join('')
+
   return (`
     <div class="garden-data-container">
       <h3><a href="/gardens/${this.id}">${this.name}</a></h3>
